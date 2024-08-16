64ac8542730f7b37fe2d77faab5e9a398e7518de
Aneesh Paul <aneesh16117@gmail.com>
2024-08-16 20:30:33 +0530

    basic setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,31 +1,9 @@
-import { auth } from "@/auth";
-import SignIn from "@/components/Signin";
-import SignOut from "@/components/Signout";
-import UserAvatar from "@/components/UserAvatar";
-import Image from "next/image";
+import Header from "@/components/Header";
+
 
 export default async function Home() {
-  const session = await auth();
-  const user = session?.user;
+
   return (
-    <div className="flex mt-5 justify-between mx-5 items-center">
-      <div className="flex gap-3 items-center">
-        {!user && <h1 className="font-bold">Next Auth Setup</h1>}
-        <h1 className="font-bold">{user?.name}</h1>
-        {/* <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} /> */}
-      </div>
-
-      <div className="flex gap-3 items-center">
-        {user ? (
-          <div>
-            <SignOut />
-          </div>
-        ) : (
-          <SignIn />
-        )}
-
-        {/* {user && <UserAvatar />} */}
-      </div>
-    </div>
+    <Header />
   );
 }
---
+++ components/Header.tsx
@@ -0,0 +1,37 @@
+import { auth } from "@/auth";
+import SignIn from "@/components/Signin";
+import SignOut from "@/components/Signout";
+import UserAvatar from "@/components/UserAvatar";
+import Image from "next/image";
+import { ModeToggle } from "./mode-toggle";
+
+const Header = async () => {
+  const session = await auth();
+  const user = session?.user;
+
+  return (
+    <div>
+      <div className="flex mt-5 justify-between mx-5 items-center">
+        <div className="flex gap-3 items-center">
+          {!user && <h1 className="font-bold">AMA</h1>}
+          <h1 className="font-bold">{user?.name}</h1>
+        </div>
+
+        <div className="flex gap-3 items-center">
+        <ModeToggle />
+          {user ? (
+            <div>
+              <SignOut />
+            </div>
+          ) : (
+            <SignIn />
+          )}
+
+          {user && <UserAvatar />}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
